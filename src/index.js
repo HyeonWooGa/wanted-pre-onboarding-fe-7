57a83dd1d7f6ad52900bf94f11589982c9ab297d
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-02 02:12:46 +0900

    ✨ 기본 다크모드 초기 설정
    Styled-Components GlobalStyle 초기화

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: ${(props) =>
+      props.darkMode ? "rgb(18, 18, 18)" : "rgb(236, 236, 236)"};
+    color: ${(props) =>
+      props.darkMode ? "rgb(236, 236, 236)" : "rgb(18, 18, 18)"};;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle darkMode />
     <App />
   </React.StrictMode>
 );
